d9371862724099b02e05aaa8b9ad3233748cfc5c
JavaScriptErika <erikajritter@gmail.com>
2018-01-18 12:45:33 -0500

    Begin adding html structure and layout style

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,8 +73,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_sass___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__app_sass__);
 
 
-console.log("hello!");
-
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
--- src/app.js
+++ src/app.js
@@ -1,3 +1,1 @@
 import './app.sass'
-
-console.log("hello!");
