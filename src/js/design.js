df7bb756a57c805e8627713fa7081811814c266e
JavaScriptErika <erikajritter@gmail.com>
2018-01-18 14:09:41 -0500

    Begin grid creation logic - almost there!

--- src/js/design.js
+++ src/js/design.js
@@ -1,1 +1,24 @@
-console.log('it works!')
+(function iife() {
+
+// set the size of the cross stitch canvas as an N by M grid with the makeGrid() function
+const makeGrid = () => {
+    var gridColumns = document.getElementById('columns').value;
+    var gridRows = document.getElementById('rows').value;
+    var gridButton = document.getElementById('grid-btn');
+    var gridTable = document.getElementById('grid-canvas')
+    var row;
+
+    gridButton.addEventListener('click', function(){
+        for(let i = 0; i < gridRows; i++) {
+            row = gridTable.insertRow(i)
+            for(let j = 0; j < gridColumns; j++) {
+                row.insertCell(j)
+            }
+        }
+    });
+    console.log(gridColumns, gridRows)
+}
+
+makeGrid();
+
+})();
