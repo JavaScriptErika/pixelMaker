6fcbd008c8c465f5331916c77db01ad503fa0ce8
JavaScriptErika <erikajritter@gmail.com>
2018-01-22 14:49:52 -0500

    Add color picker logic, refactor JS

--- src/js/design.js
+++ src/js/design.js
@@ -3,32 +3,33 @@ var gridRows = document.getElementById('rows');
 var gridButton = document.getElementById('grid-btn');
 var gridTable = document.getElementById('grid-canvas');
 var tableRows = document.getElementsByTagName('tr');
-
-
-
+var colorInput = document.getElementById('color');
+var colorInputValue = colorInput.value;
 
 var makeGrid = () => {
-    let gridRowsValue = gridRows.value;
-    let gridColumnsValue = gridColumns.value;
-    tableRows.length !== 0 ? removeRows() : addRowsAndColumns(gridRowsValue, gridColumnsValue);
+    let gridRowsInputValue = gridRows.value;
+    let gridColumnsInputValue = gridColumns.value;
+    tableRows.length !== 0 ? clearGrid() : addRowsAndColumns(gridRowsInputValue, gridColumnsInputValue);
 }
 
-
-var removeRows = () => {
+var clearGrid = () => {
     while(tableRows.length > 0) {
         tableRows[0].remove();
     }
     makeGrid();
 }
 
-var addRowsAndColumns = (gridRowsValue, gridColumnsValue) => {
+var addRowsAndColumns = (gridRowsInputValue, gridColumnsInputValue) => {
     let addRow;
-    for(let row = 0; row < gridRowsValue; row++) {
+    for(let row = 0; row < gridRowsInputValue; row++) {
         addRow = gridTable.insertRow(row);
-        for(let columns = 0; columns < gridColumnsValue; columns++) {
+        for(let columns = 0; columns < gridColumnsInputValue; columns++) {
             addRow.insertCell(columns);
         }
     }
 }
 
+window.addEventListener('load', makeGrid, false);
 gridButton.addEventListener('click', makeGrid, false);
+colorInput.addEventListener('input', () => colorInputValue = colorInput.value, false);
+gridTable.addEventListener('click', (e) => e.target.nodeName === 'TD'? e.target.style.backgroundColor = colorInputValue : '', false);
