7dbd35c526e25aeff63d8045eb746bcf9a6e9f66
JavaScriptErika <erikajritter@gmail.com>
2018-01-23 19:15:14 -0500

    remove allllll the semicolons!

--- src/js/design.js
+++ src/js/design.js
@@ -1,41 +1,41 @@
 (() => {
-    var colorInput = document.getElementById('color');
-    var gridButton = document.getElementById('grid-btn');
-    var gridColumns = document.getElementById('columns');
-    var gridRows = document.getElementById('rows');
-    var gridTable = document.getElementById('grid-canvas');
-    var tableRows = document.getElementsByTagName('tr');
+    var colorInput = document.getElementById('color')
+    var gridButton = document.getElementById('grid-btn')
+    var gridColumns = document.getElementById('columns')
+    var gridRows = document.getElementById('rows')
+    var gridTable = document.getElementById('grid-canvas')
+    var tableRows = document.getElementsByTagName('tr')
 
     var clearAndMakeGrid = () => {
-        let gridRowsInputValue = gridRows.value;
-        let gridColumnsInputValue = gridColumns.value;
-        tableRows.length !== 0 ? clearGrid() : makeGrid(gridRowsInputValue, gridColumnsInputValue);
+        let gridRowsInputValue = gridRows.value
+        let gridColumnsInputValue = gridColumns.value
+        tableRows.length !== 0 ? clearGrid() : makeGrid(gridRowsInputValue, gridColumnsInputValue)
     }
 
     var clearGrid = () => {
         while(tableRows.length > 0) {
-            tableRows[0].remove();
+            tableRows[0].remove()
         }
-        clearAndMakeGrid();
+        clearAndMakeGrid()
     }
 
     var makeGrid = (gridRowsInputValue, gridColumnsInputValue) => {
-        let addRow;
+        let addRow
         for(let row = 0; row < gridRowsInputValue; row++) {
-            addRow = gridTable.insertRow(row);
+            addRow = gridTable.insertRow(row)
             for(let columns = 0; columns < gridColumnsInputValue; columns++) {
-                addRow.insertCell(columns);
+                addRow.insertCell(columns)
             }
         }
     }
 
     var colorPicker = (e) => {
-        let colorInputValue = colorInput.value;
-        e.target.nodeName === 'TD'? e.target.style.backgroundColor = colorInputValue : '';
+        let colorInputValue = colorInput.value
+        e.target.nodeName === 'TD'? e.target.style.backgroundColor = colorInputValue : ''
     }
 
-    window.addEventListener('load', clearAndMakeGrid, colorPicker, false);
-    gridButton.addEventListener('click', clearAndMakeGrid, false);
-    gridTable.addEventListener('click', colorPicker, false);
-    colorInput.addEventListener('input', colorPicker, false);
+    window.addEventListener('load', clearAndMakeGrid, colorPicker, false)
+    gridButton.addEventListener('click', clearAndMakeGrid, false)
+    gridTable.addEventListener('click', colorPicker, false)
+    colorInput.addEventListener('input', colorPicker, false)
 })();
